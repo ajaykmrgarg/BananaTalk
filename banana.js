e09e0eb40ac489d0ca02cdf1da5da30022b73f4d
ajaykmrgarg <ajaykmrgarg@gmail.com>
2023-08-06 19:11:51 +0530

    Show output

--- banana.js
+++ banana.js
@@ -6,11 +6,12 @@ alert ("user= " + username)
 
 var btnTranslate = document.querySelector("#btn-translate")
 var txtInput = document.querySelector("#txt-input")
-
+var txtOutput=document.querySelector("#txt-output")
 
 function clickHandler(){
-    console.log("i stand clicked")
-    console.log("input", txtInput.value)
+    /*console.log("i stand clicked")*/
+    //console.log("input", txtInput.value)
+    txtOutput.innerText= "aaaa"
 }
 
 btnTranslate.addEventListener("click", clickHandler) 
