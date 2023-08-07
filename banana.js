e9bf0634edb5b515518658486f7be1be78176b9b
ajaykmrgarg <ajaykmrgarg@gmail.com>
2023-08-07 14:31:40 +0530

    final banana

--- banana.js
+++ banana.js
@@ -8,10 +8,23 @@ var btnTranslate = document.querySelector("#btn-translate")
 var txtInput = document.querySelector("#txt-input")
 var txtOutput=document.querySelector("#txt-output")
 
-function clickHandler(){
+serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(input) {
+    return serverURL +"?text=" + input
+}
+
+function clickHandler() {
     /*console.log("i stand clicked")*/
     //console.log("input", txtInput.value)
-    txtOutput.innerText= "aaaa"
-}
+    //txtOutput.innerText= "aaaa"
+
+    fetch(getTranslationURL(txtInput.value ))
+    .then(response => response.json())
+    .then (json =>{
+        //var transaltedText=json.contents.translated;
+        txtOutput.innerText=json.contents.translated
+    })
+ }
 
 btnTranslate.addEventListener("click", clickHandler) 
